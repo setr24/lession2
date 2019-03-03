6a8be517557896a2c6581b81d2544269e3925cca
Sergey Setrov <setr@setr.ru>
2019-03-03 16:55:20 +0300

    Первая версия

---
+++ z1.py
@@ -0,0 +1,12 @@
+print("Какой Ваш возраст?")
+age = int(input()) 
+
+if age <= 7:
+    print("Иди в детский сад!")
+elif age <= 18:
+ print("Пора в школу!")
+elif age <= 23:
+   print ("Пары ждут")
+
+else:
+        print("А че не на работе?")
