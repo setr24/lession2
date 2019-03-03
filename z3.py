ff204f4071d391a0f0a8534fccc65ada6ee97d9c
Sergey Setrov <setr@setr.ru>
2019-03-03 22:57:03 +0300

    Update z3.py

--- z3.py
+++ z3.py
@@ -4,8 +4,10 @@ students_scores = [{"school_clas": "4a", "scores": [1, 2, 5, 1, 4]},
 
 for score in students_scores:
     average_all = sum(score["scores"])/len(score["scores"])
-    class_sum = score["school_clas"], sum(score["scores"])/len(score["scores"])
+    print("Средний бал в школе:", average_all)
+   
+    for score_class in students_scores:
+        class_sum = score_class["school_clas"], sum(score_class["scores"])/len(score_class["scores"])
+        print("Средний бал в классе:", class_sum)
 
-print(score)
-print("Средний бал в школе:", average_all)
-print("Средний бал в классе:", class_sum)
+
